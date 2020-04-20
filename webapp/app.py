620e1b11f3f9a19a9b21e6b9a1359b0453e34226
Saket Ghai <Saket@Sakets-MacBook-Pro.local>
2020-04-20 20:36:55 +0100

    app updated

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, jsonify, json
-with open('records.json') as f:
-   all_records = json.load(f)
+from flask import Flask, request, jsonify
+from cassandra.cluster import Cluster
+
+cluster = Cluster(['cassandra'])
+session = cluster.connect()
 app = Flask(__name__)
 
-@app.route('/records', methods=['GET'])
-def get_all_records():
-   return jsonify(all_records)
+@app.route('/')
+def hello():
+    name = request.args.get("name","World")
+    str = "<p>Welcome to the cat facts API.</p> <p>You can query the api using the query like</p><p>URL/facts/10, URL/facts/5 etc</p>"
+    return(str)
 
 
-
+@app.route('/facts/<upvotes>')
+def profile(upvotes):
+    rows = session.execute( """Select * From facts.data where upvotes = '{}'""".format(upvotes))
+    if len(rows)>0:
+        return jsonify(rows)
+    else:
+        return('No record exist!')
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
